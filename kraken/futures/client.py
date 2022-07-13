edb3adafe0ef7ff6ae1698036dc03b6066a96f49
Benjamin Thomas Schwertfeger <development@b-schwertfeger.de>
2022-07-13 20:54:18 +0200

    added futures market endpoints

---
+++ kraken/futures/client.py
@@ -0,0 +1,9 @@
+from kraken.futures.market.market import MarketClient
+from kraken.futures.trade.trade import TradeClient
+
+class Trade(TradeClient):
+    pass
+
+class Market(MarketClient):
+    pass
+
---
+++ kraken/futures/trade/trade.py
@@ -0,0 +1,6 @@
+from kraken.base_api.base_api import KrakenBaseRestAPI
+
+class TradeClient(KrakenBaseRestAPI):
+
+    pass
+
