1c0dda58fe5b854d7fa7e5c0617eae3e65bcca9e
Benjamin Thomas Schwertfeger <development@b-schwertfeger.de>
2023-05-18 12:05:39 +0200

    added the Utils class that holds the new truncate function

--- kraken/spot/__init__.py
+++ kraken/spot/__init__.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Copyright (C) 2023 Benjamin Thomas Schwertfeger
-# Github: https://github.com/btschwertfeger
+# GitHub: https://github.com/btschwertfeger
 
+"""Module that provides the Spot REST clients and utility functions."""
 
-"""Module that provides the Spot REST clients"""
 # pylint: disable=unused-import
 from kraken.spot.funding import Funding
 from kraken.spot.market import Market
 from kraken.spot.staking import Staking
 from kraken.spot.trade import Trade
 from kraken.spot.user import User
+from kraken.spot.utils import Utils
 from kraken.spot.websocket import KrakenSpotWSClient
