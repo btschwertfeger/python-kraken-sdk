71c7bf3bfdbc2ecf61e05e9011186341be6a2619
Benjamin Thomas Schwertfeger <development@b-schwertfeger.de>
2023-09-03 13:46:47 +0200

    readd setup.py and requirements.txt for the doc build process

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2023 Benjamin Thomas Schwertfeger
+# GitHub: https://github.com/btschwertfeger
+#
+# This file is is required for the documentation build process.
+
+import setuptools_scm  # pylint: disable=unused-import
+from setuptools import setup
+
+setup()
