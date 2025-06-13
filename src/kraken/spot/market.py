7eda8b1ac2be13ea897a969aec12e92266199237
Benjamin T. Schwertfeger <contact@b-schwertfeger.de>
2025-06-13 06:49:16 +0200

    Update project settings (#379)

--- src/kraken/spot/market.py
+++ src/kraken/spot/market.py
@@ -1,8 +1,19 @@
 # -*- mode: python; coding: utf-8 -*-
 #
 # Copyright (C) 2023 Benjamin Thomas Schwertfeger
-# All rights reserved.
 # https://github.com/btschwertfeger
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 """Module that implements the Kraken Spot Market client"""
@@ -476,6 +487,8 @@ class Market(SpotClient):
     ) -> dict:
         """
         Returns the system status of the Kraken Spot API.
+
+        - https://docs.kraken.com/api/docs/rest-api/get-system-status
 
         :return: Success or failure
         :rtype: dict
